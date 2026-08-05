e91e96c2d7e8e207b15bb63dad20700d350037ff
Sonja Donchova <sonjadonchova@gmail.com>
2026-08-06 01:23:12 +0200

    Initial project setup

---
+++ LostAndFoundPlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LostAndFoundPlatform.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
